4d63b235064c9ee03f140a024e9c8685a9222cf2
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-09-20 08:56:10 -0700

    Commit for task 10

---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node str listint_s
+{
+	int n;
+	struct listint_s *next;
+} liucture
+ * for Holberton project
+ */
+typedef structstint_t;
+
+size_t print_listint(const listint_t *h)nt n);
+void free_listint(listint_t *head);
+int che;
+listint_t *add_nodeint(listint_t **head, const ick_cycle(listint_t *list);
+
+#endif
