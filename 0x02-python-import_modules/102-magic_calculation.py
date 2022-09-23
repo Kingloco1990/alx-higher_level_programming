19e41fc288ed4708da4414a64ca095ec622d2934
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-09-23 11:03:41 -0700

    Commit for corrections

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
+    """does task depending on bytcode"""
     from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
+        for j in range(4, 6):
+            c = add(c, j)
         return (c)
-
     else:
-        return(sub(a, b)))
+        return (sub(a, b))
