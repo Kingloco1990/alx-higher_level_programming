30086695344822a9bf524a58214753adf83495ae
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-09-28 13:31:12 -0700

    Returns a list with all values multiplied by a number without using any loops

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python3
+_keys = list(new_dir.keys())
+
+    for i in list_ke#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_d = {}
-    for i in a_dictionary:
-        new_d[i] = a_dictionary[i] * 2
-    return new_d
+    new_dir = a_dictionary.copy()
+    listys:
+        new_dir[i] *= 2
+
+    return (new_dir)
