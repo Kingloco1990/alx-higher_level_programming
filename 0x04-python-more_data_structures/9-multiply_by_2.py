ecc84194a8f996a7fc436cb651031e6e34e252d2
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-09-28 13:37:20 -0700

    Returns the weighted average of all integers tuple (<score>, <weight>)

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,9 @@
-_keys = list(new_dir.keys())
-
-    for i in list_ke#!/usr/bin/python3
+#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dir = a_dictionary.copy()
-    listys:
+    list_keys = list(new_dir.keys())
+
+    for i in list_keys:
         new_dir[i] *= 2
 
     return (new_dir)
