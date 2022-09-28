bb11cdc2e8ee8321eaab975278e8525440fa2bec
Kingloco1990 <ransfordwelbeck@gmail.com>
2022-09-28 13:45:22 -0700

    Corrections

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,11 @@ def to_subtract(list_num):
     to_sub = 0
     max_list = max(list_num)
 
-    for n in list_num:
-        if max_list > n:
-            to_sub += n
+          to_sub += n
 
-    return (max_list - to_sub)
+    return (max_list - to_sub) for n in list_num:
+        if max_list > n:
+     
 
 
 def roman_to_int(roman_string):
@@ -17,8 +17,8 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
 
-    rom_n = 500, 'M': 1000}
-    list_keys = list(rom_n.keys()){'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
 
     num = 0
     last_rom = 0
@@ -28,9 +28,9 @@ def roman_to_int(roman_string):
         for r_num in list_keys:
             if r_num == ch:
                 if rom_n.get(ch) <= last_rom:
-                   list_num = [rom_n.get(ch)]
-                els     num += to_subtract(list_num)
-                e:
+                    num += to_subtract(list_num)
+                    list_num = [rom_n.get(ch)]
+                else:
                     list_num.append(rom_n.get(ch))
 
                 last_rom = rom_n.get(ch)
