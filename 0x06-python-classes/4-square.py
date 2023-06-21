b540db55d92b2c3fe41ae535c5d7d42c215bb69a
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-06-21 01:41:45 +0000

    A class Square that defines a square by: (based on 3-square.py)

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,6 +5,7 @@ getters & setters
 
 
 class Square:
+    """Represents a square"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
