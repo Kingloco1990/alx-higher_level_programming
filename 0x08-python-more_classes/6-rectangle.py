2e7ef578417460d60f58b92244c3c8c44e7d3570
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-07-01 08:11:38 -0700

    Add number_of_instances attribute to Rectangle class

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -61,7 +61,7 @@ class Rectangle:
         """
         return self.__height
 
-    @width.setter
+    @height.setter
     def height(self, value):
         """
         Set the height of the rectangle.
