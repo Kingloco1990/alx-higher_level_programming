5dc5733f500210384fe9b6d9a9132123df538dca
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-07-02 13:46:16 -0700

    Multiplies 2 matrices

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -23,15 +23,15 @@ def matrix_mul(m_a, m_b):
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
 
-    if len(m_a) == 0 or m_a == [[]]:
-        raise ValueError("m_a can't be empty")
-    if len(m_b) == 0 or m_b == [[]]:
-        raise ValueError("m_b can't be empty")
-
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
+
+    if len(m_a) == 0 or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if len(m_b) == 0 or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
 
     if not all(isinstance(element, (int, float)) for row in m_a
                for element in row):
