4949a1bdd6bcaffbbb6542c7153f1ef6c6aabc5b
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-07-02 12:22:51 -0700

    Multiplies 2 matrices

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -43,18 +43,26 @@ def matrix_mul(m_a, m_b):
     if not all(len(row) == len(m_a[0]) for row in m_a):
         raise TypeError("each row of m_a must be of the same size")
     if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must be of the same size")
+        raise TypeError("each row of m_a must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
+    inverted_b = []
+    for r in range(len(m_b[0])):
+        new_row = []
+        for c in range(len(m_b)):
+            new_row.append(m_b[c][r])
+        inverted_b.append(new_row)
+
     new_matrix = []
-    for i in range(len(m_a)):
-        row = []
-        for j in range(len(m_b[0])):
-            sum = 0
-            for k in range(len(m_b)):
-                sum += m_a[i][k] * m_b[k][j]
-            row.append(sum)
-        new_matrix.append(row)
+    for row in m_a:
+        new_row = []
+        for col in inverted_b:
+            prod = 0
+            for i in range(len(inverted_b[0])):
+                prod += row[i] * col[i]
+            new_row.append(prod)
+        new_matrix.append(new_row)
+
     return new_matrix
