8e3e4cb7cc53f6704aac04a72129f4e8f01fdaff
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-07-02 12:50:57 -0700

    Multiplies 2 matrices

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -18,9 +18,9 @@ def matrix_mul(m_a, m_b):
 
         ValueError: If `m_a` or `m_b` is empty.
     """
-    if m_a == [] or m_a == [[]]:
+    if len(m_a) == 0 or m_a == [[]]:
         raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]]:
+    if len(m_b) == 0 or m_b == [[]]:
         raise ValueError("m_b can't be empty")
 
     if not isinstance(m_a, list):
@@ -33,20 +33,22 @@ def matrix_mul(m_a, m_b):
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
 
-    if not all((isinstance(ele, int) or isinstance(ele, float))
-               for ele in [num for row in m_a for num in row]):
+    if not all(isinstance(element, (int, float)) for row in m_a
+               for element in row):
         raise TypeError("m_a should contain only integers or floats")
-    if not all((isinstance(ele, int) or isinstance(ele, float))
-               for ele in [num for row in m_b for num in row]):
+
+    if not all(isinstance(element, (int, float)) for row in m_b
+               for element in row):
         raise TypeError("m_b should contain only integers or floats")
 
-    if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must should be of the same size")
-    if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must should be of the same size")
+    if len(set(len(row) for row in m_a)) > 1:
+        raise TypeError("each row of m_a must be of the same size")
+
+    if len(set(len(row) for row in m_b)) > 1:
+        raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
+        raise TypeError("m_a and m_b can't be multiplied")
 
     new_matrix = []
     for i in range(len(m_a)):
