509b46fe7296e70db4ac941a6247a67dc4227b59
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-07-05 15:52:45 -0700

    A class MyInt that inherits from int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
-"""
-Contains definition of class MyInt
-"""
+"""This module contains a custom class, MyInt, which inherits from int."""
 
 
 class MyInt(int):
-    """Definition of class MyInt that inherits from class int"""
-
+    """
+    A custom class that inherits from int and overrides
+    comparison operators.
+    """
     def __eq__(self, other):
-        """Overrides equals, inverting it"""
-        return int(self) != int(other)
+        """
+        Override the equality operator.
+
+        Args:
+            other (Any): The object to compare with.
+
+        Returns:
+            bool: True if the values are not equal, False otherwise.
+        """
+        return not super().__eq__(other)
 
     def __ne__(self, other):
-        """Overrides not-equals, inverting it"""
-        return int(self) == int(other)
+        """
+        Override the inequality operator.
+
+        Args:
+            other (Any): The object to compare with.
+
+        Returns:
+            bool: True if the values are equal, False otherwise.
+        """
+        return not super().__ne__(other)
