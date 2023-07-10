96db03113258c012674ba57be54e8d9c1bca65f5
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-07-10 19:42:44 +0000

    Appends a new string after a specified search string in a file

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
-"""Defines a text file insertion function."""
+"""
+This module provides a function that appends a new string after
+a specified search string in a file.
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Insert text after each line containing a given string in a file.
-    Args:
-        filename (str): The name of the file.
-        search_string (str): The string to search for within the file.
-        new_string (str): The string to insert.
     """
+    Appends a new string after a specified search string in a file.
+
+Args:
+    filename (str, optional): The name of the file to modify.
+                            Defaults to an empty string.
+    search_string (str, optional): The string to search for in the file.
+                                   Defaults to an empty string.
+    new_string (str, optional): The string to append after the search string.
+                                Defaults to an empty string.
+    """
+
+    # Read the contents of the file
     text = ""
-    with open(filename) as r:
-        for line in r:
+    with open(filename) as r_file:
+        for line in r_file:
             text += line
             if search_string in line:
                 text += new_string
-    with open(filename, "w") as w:
-        w.write(text)
+
+    # Write the modified contents back to the file
+    with open(filename, "w") as w_file:
+        w_file.write(text)
