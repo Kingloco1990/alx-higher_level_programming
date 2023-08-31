7388ca2eb5b05e2d2322dcc424efb3b00a780cd4
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-08-31 23:34:59 +0000

    Header file for the project

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,22 +4,22 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node str listint_s
+* struct listint_s - singly linked list
+* @n: integer
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+*
+*/
+typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-} liucture
- * for Holberton project
- */
-typedef structstint_t;
+} listint_t;
 
-size_t print_listint(const listint_t *h)nt n);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
-int che;
-listint_t *add_nodeint(listint_t **head, const ick_cycle(listint_t *list);
+int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
