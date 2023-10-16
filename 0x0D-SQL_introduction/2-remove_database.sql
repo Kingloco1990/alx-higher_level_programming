3c3c8edce25f3f991a898a422889a17d5f30c1c0
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-10-16 21:14:58 +0000

    Deletes database named 'hbtn_0c_0' if it exists

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
--- Deletes the database hbtn_0c_0 if it exists in your MySQL server.
+-- Deletes the database named 'hbtn_0c_0' if it exists in your MySQL server.
 DROP DATABASE IF EXISTS `hbtn_0c_0`;
