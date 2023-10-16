49db344d425b79357b58363af1b782b2ab8e9832
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-10-16 21:33:25 +0000

    Creates a table named 'first_table' in the current database

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
--- creates a table named 'first_table' in the current database in my MySQL server.
+-- Creates a table named 'first_table' in the current database in my MySQL server.
 CREATE TABLE IF NOT EXIST first_table (
   id INT,
   name VARCHAR(256)
