d32699a0ad15c7475064121380d092f40323687e
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-11-17 13:09:06 +0000

    Converts hbtn_0c_0 database to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,6 +4,3 @@ ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Convert the table to UTF8
 ALTER TABLE `hbtn_0c_0`.`first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the field 'name' in the table to UTF8
-ALTER TABLE `hbtn_0c_0`.`first_table` MODIFY COLUMN name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
