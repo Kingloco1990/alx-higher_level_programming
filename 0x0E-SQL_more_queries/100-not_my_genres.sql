08c7386222e99236dc1de1796203db0e3d8c2984
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-11-20 23:56:17 +0000

    Lists genres in 'hbtn_0d_tvshows' not linked to Dexter

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -6,7 +6,8 @@ FROM tv_genres AS g
 INNER JOIN tv_show_genres AS s ON g.id = s.genre_id
 INNER JOIN tv_shows AS t ON s.show_id = t.id
 WHERE g.name NOT IN (
-    SELECT name FROM tv_genres AS g
+    SELECT name
+    FROM tv_genres AS g
     INNER JOIN tv_show_genres AS s ON g.id = s.genre_id
     INNER JOIN tv_shows AS t ON s.show_id = t.id
 	WHERE t.title = "Dexter")
