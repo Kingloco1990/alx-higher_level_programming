f6462a0d1e0c2d1c0065fd136724585680f4173e
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-12-17 22:53:43 +0000

    Lists all cities of a specified state from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
-'''script for task 5'''
-
-import MySQLdb
+"""
+Lists all cities of a specified state from the database hbtn_0e_4_usa
+"""
 import sys
-
-
-def list_by_state():
-    '''lists all cities of a state passed as argument to the script'''
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-    host = 'localhost'
-    port = 3306
-
-    db = MySQLdb.connect(host=host, user=username, passwd=password,
-                         db=db_name, port=port)
-    cur = db.cursor()
-    cur.execute('SELECT c.name FROM cities c INNER JOIN states s ' +
-                'ON s.id = c.state_id WHERE ' +
-                'BINARY s.name = %s ' +
-                'ORDER BY c.id ASC;', [state_name])
-    result = cur.fetchall()
-    cur.close()
-    db.close()
-
-    print(', '.join(map(lambda x: x[0], result)))
-
+import MySQLdb
 
 if __name__ == '__main__':
-    list_by_state()
+    # Connect to the MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Execute the query to retrieve cities of the specified state
+    cur.execute(
+        "SELECT cities.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id",
+        (sys.argv[4],)
+    )
+
+    # Fetch all the results
+    cities = cur.fetchall()
+
+    # Display the results
+    if cities:
+        city_names = ', '.join(city[0] for city in cities)
+        print(city_names)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
