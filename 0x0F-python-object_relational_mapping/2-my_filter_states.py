9e8fda2e19f22dc74ecede9de6142f3c56669fba
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-12-17 21:07:55 +0000

    Displays all values in the states table where name matches an argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,37 +1,27 @@
 #!/usr/bin/python3
-''' script for task 2
-    script should take 4 arguments: mysql username,
-    mysql password, database name and state name searched
 '''
-
-import MySQLdb
+Takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+'''
 import sys
-
-
-def list_with_name():
-    ''' displays all values in the states table in hbtn db where name
-        matches the argument passed to the script
-    '''
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-    host = 'localhost'
-    port = 3306
-
-    db = MySQLdb.connect(host=host, user=username, passwd=password,
-                         db=db_name, port=port)
-    cur = db.cursor()
-    cur.execute(('SELECT * FROM states WHERE BINARY name = \'{}\'\
-                 ORDER BY id ASC;').format(state_name))
-    result = cur.fetchall()
-    cur.close()
-    db.close()
-
-    if result:
-        for row in result:
-            print(row)
-
+import MySQLdb
 
 if __name__ == '__main__':
-    list_with_name()
+    # Connect to the MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    # Create a cursor object
+    cur = db.cursor()
+
+    # Prepare the SQL query using format with user input
+    query = "SELECT * FROM states\
+            WHERE name = '{}'\
+            ORDER BY id".format(sys.argv[4])
+
+    # Execute the SQL query
+    cur.execute(query)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
