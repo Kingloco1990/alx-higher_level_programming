cf3f8db07903716bf643d8e5b7394c4250eadecd
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-12-17 19:07:27 +0000

    Lists all states starting with 'N' from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
 
+
     # Create a cursor object
     cur = db.cursor()
 
