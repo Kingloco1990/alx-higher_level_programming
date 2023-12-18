e5fdc9e0d04249923d040e3879e86ca04798c0d3
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-12-18 12:27:44 +0000

    Contains the State class and declarative base instance

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Contains the class definition of a State and
+an instance Base = declarative_base().
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+# Create an instance of the declarative base class
+Base = declarative_base()
+
+
+# Define the State class, which inherits from Base
+class State(Base):
+    """
+    Represents the State class corresponding to the 'states' table
+    in the database.
+
+    Attributes:
+        id (int): An auto-generated, unique integer serving as
+        the primary key.
+
+        name (str): A string column with a maximum length of 128 characters,
+        cannot be null.
+    """
+
+    # Define the name of the corresponding database table
+    __tablename__ = 'states'
+
+    # Define columns for the 'states' table
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
