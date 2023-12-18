5354650e86bb5b475cd73bc530c47754f3e7c65b
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-12-18 17:51:58 +0000

    Defines the State class

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Defines the State class and inherites from SQLAlchemy Base.
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from relationship_city import Base, City
+
+# Create an instance of the declarative base class
+Base = declarative_base()
+
+
+# Define the State class, which inherits from Base
+class State(Base):
+    """
+    Represents the State class corresponding to the 'states' table
+    in the database.
+
+    Attributes:
+        id (int): An auto-generated, unique integer serving as
+        the primary key.
+
+        name (str): A string column with a maximum length of 128 characters,
+        cannot be null.
+
+        cities (relationship): A one-to-many relationship with the City class,
+        establishing a link between states and cities in the database.
+    """
+
+    # Define the name of the corresponding database table
+    __tablename__ = 'states'
+
+    # Define columns for the 'states' table
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship('City', backref='state', cascade='all, delete-orphan')
