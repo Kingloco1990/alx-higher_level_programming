09730b79583adab091a077304f0889db7f5b1df4
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-12-18 17:55:00 +0000

    Creates the State 'California' with the City 'San Francisco'

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
-""" Script that creates the State “California” with the City
-    “San Francisco” from the database hbtn_0e_100_usa """
+"""
+Creates the State "California" with the City "San Francisco"
+in the database hbtn_0e_100_usa.
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State, Base
+from relationship_city import City
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    # Get database credentials from command line arguments
+    username, password, db_name = sys.argv[1:4]
 
-    import sys
-    from relationship_state import State, Base
-    from relationship_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from sqlalchemy.schema import Table
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+    # Create an engine to connect to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    s = Session(engine)
-    news = State(name='California')
-    newc = City(name='San Francisco')
-    news.cities.append(newc)
-    s.add_all([news, newc])
-    s.commit()
-    s.close()
+    # Create California State and San Francisco City
+    california = State(name='California', cities=[City(name='San Francisco')])
+    session.add(california)
+    session.commit()
+
+    # Close the session
+    session.close()
