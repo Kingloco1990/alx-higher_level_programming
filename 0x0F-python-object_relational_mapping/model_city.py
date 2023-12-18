7f3dac22eb6971d3c08386a7a01fe16763195186
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-12-18 15:16:27 +0000

    Contains the City class and declarative base instance

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Contains the class definition of a City and
+an instance Base = declarative_base().
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+# Create an instance of the declarative base class
+Base = declarative_base()
+
+
+# Define the State class, which inherits from Base
+class City(Base):
+    """
+    Represents the State class corresponding to the 'states' table
+    in the database.
+
+    Attributes:
+        id (int): An auto-generated, unique integer serving as
+        the primary key.
+
+        name (str): A string column with a maximum length of 128 characters,
+        cannot be null.
+        
+        state_id (int): An integer column representing the foreign key
+        referencing the 'id' column in the 'states' table.
+    """
+
+    # Define the name of the corresponding database table
+    __tablename__ = 'cities'
+
+    # Define columns for the 'states' table
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+    states_id = Column(Integer, ForeignKey('states.id'), nullable=False)
