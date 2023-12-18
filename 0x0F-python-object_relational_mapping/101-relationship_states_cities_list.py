91d4990d676684b5dc034985525dd58a94d284dc
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-12-18 17:59:01 +0000

    Lists all State objects and corresponding City objects

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
 """
-Lists all State objects and corresponding City objects contained in the DB
+Lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa.
 """
 import sys
-from relationship_state import Base, State
-from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from relationship_state import State, Base
+from relationship_city import City
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    # Get database credentials from command line arguments
+    username, password, db_name = sys.argv[1:4]
+
+    # Create an engine to connect to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    st = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+    # Query for all State objects with cities, sorted by states.id and cities.id
+    states_cities = session.query(State).order_by(State.id).all()
 
-    for state in st:
-        print("{}: {}".format(state.id, state.name))
+    # Print the results
+    for state in states_cities:
+        print('{}: {}'.format(state.id, state.name))
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+            print('\t{}: {}'.format(city.id, city.name))
+
+    # Close the session
+    session.close()
