a192ae928766cb96093f4224ef5f848a8e80e2ba
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-12-18 17:08:57 +0000

    Prints all City objects from the database hbtn_0e_14_usa

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
-# Lists all City objects from the database hbtn_0e_14_usa.
-# Usage: ./14-model_city_fetch_by_state.py <mysql username> /
-#                                          <mysql password> /
-#                                          <database name>
+"""
+Prints all City objects from the database hbtn_0e_14_usa.
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 from model_city import City
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+if __name__ == '__main__':
+    # Get database credentials from command line arguments
+    username, password, db_name = sys.argv[1:4]
+
+    # Create an engine to connect to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
                            pool_pre_ping=True)
+
+    # Create a Session class for interacting with the database
     Session = sessionmaker(bind=engine)
+
+    # Establish a new session to execute queries
     session = Session()
 
-    for city, state in session.query(City, State) \
-                              .filter(City.state_id == State.id) \
-                              .order_by(City.id):
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    # Query for City objects and display the results
+    cities = session.query(State, City).filter(State.id == City.state_id)\
+                    .order_by(City.id).all()
+
+    # Print the results
+    for state, city in cities:
+        print('{}: ({}) {}'.format(state.name, city.id, city.name))
+
+    # Close the session
+    session.close()
