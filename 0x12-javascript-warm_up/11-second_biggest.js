7113ff6ffda114576cba7a19e275fb3fff95233a
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-12-30 20:16:49 +0000

    Searches the second biggest integer in the list of arguments

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
+// Check if the number of command line arguments is less than or equal to 3
 if (process.argv.length <= 3) {
+  // If so, print 0 and exit the script
   console.log(0);
 } else {
+  // If there are more than 3 command line arguments
+  // Map the command line arguments to Numbers
   const args = process.argv.map(Number)
+    // Slice the array to exclude the first two elements (script name and interpreter)
     .slice(2, process.argv.length)
+    // Sort the array in ascending order
     .sort((a, b) => a - b);
+
+  // Print the second-to-last element of the sorted array
   console.log(args[args.length - 2]);
 }
