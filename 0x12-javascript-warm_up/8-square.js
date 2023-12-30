01c9a0bb68b47a4e0852390b677486bcf2ea211c
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-12-30 12:46:35 +0000

    Prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
+const size = parseInt(process.argv[2]);
+let yCounter = 0;
 
-const { argv } = require('process');
-const size = parseInt(argv[2]);
-
-const printSquare = (size) => {
-  const row = 'X'.repeat(size);
-  for (let i = 0; i < size; i++) console.log(row);
-};
-
-Number.isInteger(size) ? printSquare(size) : console.log('Missing size');
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  while (yCounter < size) {
+    let line = '';
+    for (let xCounter = 0; xCounter < size; xCounter++) {
+      line += 'X';
+    }
+    console.log(line);
+    yCounter++;
+  }
+}
