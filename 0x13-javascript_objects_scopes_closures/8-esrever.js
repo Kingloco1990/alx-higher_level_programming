1a3b5156c244dac1e4809f2a9c67a8d20db3c045
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-01-02 16:04:04 +0000

    Returns the reversed version of a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,17 @@
 #!/usr/bin/node
+
 exports.esrever = function (list) {
-  return list.reduceRight(function (array, current) {
-    array.push(current);
-    return array;
-  }, []);
+  // Clone the original list to avoid modifying it
+  // Create a new array by slicing the original list
+  let reversedList = list.slice();
+
+  // Perform in-place reversal
+  // Use a loop to swap elements at positions i and j
+  for (let i = 0, j = reversedList.length - 1; i < j; i++, j--) {
+    // Swap elements using destructuring assignment
+    [reversedList[i], reversedList[j]] = [reversedList[j], reversedList[i]];
+  }
+
+  // Return the reversed list
+  return reversedList;
 };
