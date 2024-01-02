4138d894f7077b9f338b0247cc8007592cf94821
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-01-02 15:49:52 +0000

    Returns the number of times a number occurs in a list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,13 @@
 #!/usr/bin/node
+
 exports.nbOccurences = function (list, searchElement) {
-  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
+  // Use the reduce function to count occurrences
+  const occurrences = list.reduce((count, element) => {
+    // Increment count when the current element matches the searchElement
+    // If the current element matches 'searchElement', add 1 to the count; otherwise, add 0
+    return count + (element === searchElement ? 1 : 0);
+  }, 0);
+
+  // Return the total count of occurrences
+  return occurrences;
 };
