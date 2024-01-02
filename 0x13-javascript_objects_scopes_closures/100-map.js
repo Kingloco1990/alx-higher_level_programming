1d6f0dd98d874085794eb9b744f660ee6b5675c6
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-01-02 16:32:03 +0000

    Imports an array and computes a new array

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 
 // Import the 'list' array from the file 100-data.js
-const { list } = require('./100-data');
+const list = require('./100-data').list;
 
 // Uses the map function to create a new list where each value is multiplied by its index
-const newList = list.map((value, index) => value * index);
+const newList = list.map(function (num, index) {
+  return num * index;
+});
 
 // Prints the initial list
 console.log(list);
