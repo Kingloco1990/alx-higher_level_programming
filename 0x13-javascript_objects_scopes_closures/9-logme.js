1f792bef4ab2073a40e4ed5cf0ff53802b200df1
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-01-02 16:19:46 +0000

    Prints the number of arguments already printed and the argument value

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-// Initialize a counter for the number of arguments printed
+// Initializes a counter for the number of arguments printed
 let argsPrinted = 0;
 
 exports.logMe = function (item) {
-  // Log the number of arguments already printed and the current argument value
+  // Logs the number of arguments already printed and the current argument value
   console.log(`${argsPrinted}: ${item}`);
 
-  // Increment the counter after printing each argument
+  // Increments the counter after printing each argument
   argsPrinted++;
 };
