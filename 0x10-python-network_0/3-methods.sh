6224cecde029bc07b5a19add5101d24545484ec6
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-04-04 18:49:44 +0000

    Displays all HTTP methods the server of a URL will accept

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays all HTTP methods the server of a given URL will accept.
+# Displays all HTTP methods the server of a URL will accept.
 curl -sI "$1" | grep '^Allow:' | sed 's/Allow: //'
