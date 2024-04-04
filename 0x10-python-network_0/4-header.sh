68f34359b7a39688993b946100c92b2cdf015223
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-04-04 18:57:42 +0000

    Sends a GET request with a header variable and displays the response body

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
+# Displays the response body after sending a GET request with a header variable to a URL.
 curl -sH "X-School-User-Id: 98" "$1"
