73baf08109a5c55585224f17c5074b9c299ecb03
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-04-07 10:31:19 +0000

    Sends a POST request to a URL with JSON data and displays response body

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a POST request to a URL with JSON data and displays the response body.
+# Sends a POST request to a URL (with JSON data) and displays the response body.
 curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
