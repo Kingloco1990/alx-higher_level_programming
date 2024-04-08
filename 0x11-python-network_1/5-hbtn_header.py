e8b085aa3c7b5cec7c9e121e356208fc9b852529
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-04-08 19:20:05 +0000

    Fetches and prints the value of X-Request-Id variable using requests

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL.
-
-Usage: ./5-hbtn_header.py <URL>
 """
-import sys
+This script sends a GET request to a specified URL provided as a
+command-line argument and prints the value of the
+'X-Request-Id' header from the response.
+"""
 import requests
+import sys
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    # Extract the URL from the command-line arguments
     url = sys.argv[1]
 
-    r = requests.get(url)
-    print(r.headers.get("X-Request-Id"))
+    # Send a GET request to the specified URL
+    response = requests.get(url)
+
+    # Extract the value of the 'X-Request-Id' header from the response
+    x_request_id = response.headers.get('X-Request-Id')
+
+    # Print the value of the 'X-Request-Id' header
+    print(x_request_id)
