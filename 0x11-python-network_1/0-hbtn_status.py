381ec64760929e3e58c3d067427e0f63e25c2d87
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-04-08 15:02:41 +0000

    Fecthes https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
 """
+This script fetches https://alx-intranet.hbtn.io/status using
+the urllib package.
+"""
+import urllib.request
 
 
 if __name__ == '__main__':
-    import urllib.request
+    # Specify the URL
+    url = 'https://alx-intranet.hbtn.io/status'
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
+    # Send a GET request to the URL
+    with urllib.request.urlopen(url) as response:
+        # Read the response body
+        body = response.read()
+
+        # Decode the response body as UTF-8
+        utf8_content = body.decode('utf-8')
+
+        # Display information about the response body
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", utf8_content)
