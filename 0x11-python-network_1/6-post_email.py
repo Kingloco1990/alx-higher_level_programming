aa10207de1c09b5a5108906b32724974e8a85410
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-04-08 19:23:46 +0000

    Sends a POST request with an email and displays response body

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-
-Usage: ./6-post_email.py <URL> <email>
-  - Displays the body of the response.
 """
-import sys
+This script sends a POST request to a specified URL with an email parameter
+provided as a command-line argument and prints the body of the response.
+"""
 import requests
+import sys
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    # Extract the URL and email from the command-line arguments
     url = sys.argv[1]
-    value = {"email": sys.argv[2]}
+    email = sys.argv[2]
 
-    r = requests.post(url, data=value)
-    print(r.text)
+    # Send a POST request to the specified URL with the email as a parameter
+    response = requests.post(url, data={'email': email})
+
+    # Print the body of the response
+    print(response.text)
