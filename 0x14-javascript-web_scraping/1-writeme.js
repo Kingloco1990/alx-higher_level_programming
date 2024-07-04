79241313c0fdd05cc08af7376f6d5ea60ba0d2de
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-07-04 17:44:02 +0000

    Writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
+
+// Import the 'fs' module to interact with the file system
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) console.log(error);
-});
+
+// Get the file name and content from command line arguments
+const file = process.argv[2];
+const content = process.argv[3];
+
+try {
+  // Write the content to the specified file with 'utf-8' encoding
+  fs.writeFileSync(file, content, 'utf-8');
+} catch (err) {
+  // Log any errors that occur during the write process
+  console.error(err);
+}
