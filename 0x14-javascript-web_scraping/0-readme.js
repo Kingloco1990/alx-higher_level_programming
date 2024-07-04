7a8ed3c7c4ddae88cc662ab335d00492a7a2a971
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-07-04 17:33:06 +0000

    Reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
-});
+const file = process.argv[2];
+
+try {
+  const data = fs.readFileSync(file, 'utf-8');
+  console.log(data);
+} catch (err) {
+  console.error(err);
+}
