0dc106924a4efa85536822b11d1f94f41f015acf
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-07-04 18:21:25 +0000

    Displays the status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2]).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
+
+// Get the URL from command-line arguments
+const url = process.argv[2];
+
+// Make an HTTP GET request to the provided URL
+request(url, function(error, response) {
+    // Check if there's an error during the request
+    if (error) {
+        console.error(error); // Log the error to the console
+    } else {
+        // Log the HTTP status code of the response to the console
+        console.log(`code: ${response.statusCode}`);
+    }
 });
