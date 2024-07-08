4a2bbacadcb6c1c12421c49d8d527a96ecf40c5a
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-07-08 20:46:19 +0000

    Prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,21 +1,40 @@
 #!/usr/bin/node
 
-const req = require('request');
-const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
-req.get(url + id, function (error, res, body) {
+// Import the 'request' module to make HTTP requests
+const request = require('request');
+
+// Get the movie ID from the command-line arguments
+const movieId = process.argv[2];
+
+// Construct the URL of the Star Wars API for movies using the movieId
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+// Make an HTTP GET request to fetch the movie data from apiUrl
+request(apiUrl, function (error, response, body) {
+  // Check if there's an error during the request
   if (error) {
-    console.log(error);
+    console.error('Request failed:', error);
+    return;
   }
-  const data = JSON.parse(body);
-  const dd = data.characters;
-  for (const i of dd) {
-    req.get(i, function (error, res, body1) {
+
+  // Parse the response body as JSON to get movie details
+  const movie = JSON.parse(body);
+
+  // Print each character's name who appeared in the movie
+  movie.characters.forEach(characterUrl => {
+    // Make an HTTP GET request to fetch details of each character
+    request(characterUrl, function (error, response, body) {
+      // Check if there's an error during the request for character details
       if (error) {
-        console.log(error);
+        console.error('Request failed:', error);
+        return;
       }
-      const data1 = JSON.parse(body1);
-      console.log(data1.name);
+
+      // Parse the response body as JSON to get character details
+      const character = JSON.parse(body);
+
+      // Print the name of the character
+      console.log(character.name);
     });
-  }
+  });
 });
