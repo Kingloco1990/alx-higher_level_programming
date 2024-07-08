4928c8143a39bfba112d235e76af6470919a434d
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-07-08 20:33:41 +0000

    Computes completed tasks by user ID

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,39 @@
 #!/usr/bin/node
 
+// Import the 'request' module to make HTTP requests
 const request = require('request');
-const url = process.argv[2];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
+// Get the API URL from the command-line arguments (third argument)
+const apiUrl = process.argv[2];
+
+// Make an HTTP GET request to the provided API URL
+request(apiUrl, function (error, response, body) {
+  // Check if there's an error during the request
+  if (error) {
+    console.error('Request failed:', error);
+    return;
+  }
+
+  // Parse the response body as JSON
+  const todos = JSON.parse(body);
+
+  // Initialize an object to store the count of completed tasks by user ID
+  const completedTasks = {};
+
+  // Loop through each task (todo) in the todos array
+  todos.forEach(todo => {
+    // Check if the task is marked as completed
+    if (todo.completed) {
+      // If the user ID already exists in completedTasks, increment the count
+      if (completedTasks[todo.userId]) {
+        completedTasks[todo.userId]++;
+      } else {
+        // If it doesn't exist, initialize it with a count of 1
+        completedTasks[todo.userId] = 1;
       }
     }
-    console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
-  }
+  });
+
+  // Print the completedTasks object, which now contains counts of completed tasks by user ID
+  console.log(completedTasks);
 });
