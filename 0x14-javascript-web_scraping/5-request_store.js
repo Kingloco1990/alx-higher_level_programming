405aa7d17efb2435342e070bf52960ced3f211e6
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-07-08 20:12:16 +0000

    Fetches webpage content and store in file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,28 @@
 #!/usr/bin/node
-const fs = require('fs');
+
+// Import the 'request' module to make HTTP requests
 const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+
+// Import the 'fs' module to handle file system operations
+const fs = require('fs');
+
+// Get the URL and file path from the command-line arguments
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Make an HTTP GET request to the provided URL
+request(url, function (error, response, body) {
+  // Check if there's an error during the request
+  if (error) {
+    console.error('Request failed:', error);
+    return;
+  }
+
+  // Attempt to write the response body to the specified file
+  fs.writeFile(filePath, body, 'utf8', function (err) {
+    // Log only if there is an error during the file write operation
+    if (err) {
+      console.error('Failed to write to file:', err);
+    }
+  });
+});
