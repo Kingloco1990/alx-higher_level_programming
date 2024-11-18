b923d9f522a6bc46ddcebe7675abd2ff0a0fe792
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-11-18 20:59:39 +0000

    Fetches and displays Hello based on language code

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,7 +1,8 @@
-$(document).ready(function () {
+$('document').ready(function () {
   $('INPUT#btn_translate').click(function () {
-    const languageCode = $('INPUT#language_code').val(); // Get the value of the language code entered
-    $.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`, function (data) {
+    const langCode = $('INPUT#language_code').val(); // Get the value of the language code entered
+    const url = `https://www.fourtonfish.com/hellosalut/?lang=${langCode}`;
+    $.get(url, function (data) {
       $('DIV#hello').text(data.hello); // Display the translation of "Hello"
     });
   });
