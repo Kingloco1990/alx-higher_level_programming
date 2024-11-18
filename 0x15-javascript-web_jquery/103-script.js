43249762b33ec4ffd1cf336acbf7c56b62002add
Kingloco1990 <ransfordnawelbeck@gmail.com>
2024-11-18 15:32:10 +0000

    Fetches and prints Hello based on language code

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,17 +1,22 @@
 $('document').ready(function () {
+  const url = 'https://www.fourtonfish.com/hellosalut/?';
+
+  function translate() {
+    const langCode = $('INPUT#language_code').val();
+    if (langCode) {
+      $.get(url + $.param({ lang: langCode }), function (data) {
+        $('DIV#hello').html(data.hello);
+      });
+    }
+  }
+
+  // Trigger translate on button click
   $('INPUT#btn_translate').click(translate);
-  $('INPUT#language_code').focus(function () {
-    $(this).keydown(function (e) {
-      if (e.keyCode === 13) {
-        translate();
-      }
-    });
+
+  // Trigger translate on pressing Enter in the input field
+  $('INPUT#language_code').keypress(function (e) {
+    if (e.keyCode === 13) { // 13 is the Enter key
+      translate();
+    }
   });
 });
-
-function translate () {
-  const url = 'https://www.fourtonfish.com/hellosalut/?';
-  $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
-    $('DIV#hello').html(data.hello);
-  });
-}
